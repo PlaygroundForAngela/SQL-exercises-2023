475466eeaec2a47d640afe6d2553b3d3629c0cfb
Angela codes for fun <91817457+PlaygroundForAngela@users.noreply.github.com>
2023-07-19 15:46:08 +0800

    Generate lists of countries
    DATABASE

---
+++ Generate lists of countries.sql
@@ -0,0 +1,404 @@
+USE WorldEvents
+GO
+
+CREATE TABLE CountriesByArea(
+	Position int NOT NULL PRIMARY KEY,
+	Country nvarchar(255) NULL,
+	KmSquared bigint NULL)
+
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (1, N'Russia', 17098246)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (2, N'Canada', 9984670)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (3, N'China', 9572900)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (4, N'United States of America', 9525067)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (5, N'Brazil', 8515767)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (6, N'Australia', 7692024)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (7, N'India', 3287263)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (8, N'Argentina', 2780400)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (9, N'Kazakhstan', 2724900)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (10, N'Algeria', 2381740)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (11, N'Democratic Republic of the Congo', 2345410)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (12, N'Kingdom of Denmark', 2210583)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (13, N'Saudi Arabia', 2149690)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (14, N'Mexico', 1964375)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (15, N'Indonesia', 1904556)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (16, N'Sudan', 1861484)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (17, N'Libya', 1759540)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (18, N'Iran', 1648000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (19, N'Mongolia', 1565000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (20, N'Peru', 1285220)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (21, N'Chad', 1284000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (22, N'Niger', 1267000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (23, N'Angola', 1246700)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (24, N'Mali', 1240000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (25, N'South Africa', 1219912)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (26, N'Colombia', 1197411)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (27, N'Ethiopia', 1127127)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (28, N'Bolivia', 1098580)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (29, N'Mauritania', 1030700)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (30, N'Egypt', 1001450)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (31, N'Tanzania', 945087)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (32, N'Nigeria', 923768)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (33, N'Venezuela', 912050)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (34, N'Namibia', 825418)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (35, N'Pakistan', 803940)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (36, N'Mozambique', 801590)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (37, N'Turkey', 780580)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (38, N'Chile', 756950)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (39, N'Zambia', 752614)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (40, N'Myanmar (Burma)', 678500)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (41, N'France', 675417)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (42, N'Afghanistan', 647500)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (43, N'Somalia', 637657)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (44, N'Central African Republic', 622984)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (45, N'South Sudan', 619745)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (46, N'Ukraine', 603628)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (47, N'Botswana', 600370)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (48, N'Madagascar', 587040)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (49, N'Kenya', 580367)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (50, N'Yemen', 527970)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (51, N'Thailand', 514000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (52, N'Spain', 504781)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (53, N'Turkmenistan', 488100)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (54, N'Cameroon', 475440)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (55, N'Papua New Guinea', 462840)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (56, N'Sweden', 449964)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (57, N'Uzbekistan', 447400)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (58, N'Morocco', 446550)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (59, N'Iraq', 437072)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (60, N'Paraguay', 406750)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (61, N'Zimbabwe', 390580)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (62, N'Japan', 377835)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (63, N'Germany', 357021)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (64, N'Republic of the Congo', 342000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (65, N'Finland', 337030)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (66, N'Malaysia', 329750)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (67, N'Vietnam', 329560)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (68, N'Norway', 324220)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (69, N'Ivory Coast', 322460)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (70, N'Poland', 312685)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (71, N'Oman', 309500)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (72, N'Italy', 301230)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (73, N'Philippines', 300000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (74, N'Ecuador', 283560)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (75, N'Burkina Faso', 274200)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (76, N'New Zealand', 269190)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (77, N'Gabon', 267667)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (78, N'Guinea', 245857)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (79, N'United Kingdom', 243610)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (80, N'Uganda', 241550)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (81, N'Ghana', 238540)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (82, N'Romania', 238391)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (83, N'Laos', 236800)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (84, N'Guyana', 214970)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (85, N'Belarus', 207600)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (86, N'Kyrgyzstan', 198500)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (87, N'Senegal', 196190)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (88, N'Syria', 185180)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (89, N'Cambodia', 181035)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (90, N'Uruguay', 176220)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (91, N'Tunisia', 163610)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (92, N'Suriname', 163270)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (93, N'Bangladesh', 147570)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (94, N'Nepal', 147181)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (95, N'Tajikistan', 143100)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (96, N'Greece', 131940)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (97, N'Nicaragua', 129494)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (98, N'Eritrea', 121320)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (99, N'North Korea', 120540)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (100, N'Malawi', 118480)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (101, N'Benin', 112620)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (102, N'Honduras', 112090)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (103, N'Liberia', 111370)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (104, N'Bulgaria', 110910)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (105, N'Cuba', 109886)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (106, N'Guatemala', 108890)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (107, N'Iceland', 103000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (108, N'South Korea', 98480)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (109, N'Hungary', 93030)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (110, N'Jordan', 92300)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (111, N'Portugal', 91188)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (112, N'Serbia', 88361)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (113, N'Azerbaijan', 86600)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (114, N'Austria', 83858)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (115, N'United Arab Emirates', 82880)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (116, N'Czech Republic', 78867)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (117, N'Panama', 78201)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (118, N'Sierra Leone', 72740)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (119, N'Ireland', 71273)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (120, N'Georgia', 69701)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (121, N'Sri Lanka', 65611)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (122, N'Lithuania', 65201)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (123, N'Latvia', 64589)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (124, N'Togo', 56785)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (125, N'Croatia', 56542)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (126, N'Bosnia and Herzegovina', 51129)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (127, N'Costa Rica', 51100)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (128, N'Slovakia', 48845)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (129, N'Dominican Republic', 48730)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (130, N'Bhutan', 47000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (131, N'Estonia', 45339)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (132, N'Netherlands', 42679)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (133, N'Switzerland', 41210)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (134, N'Guinea-Bissau', 36120)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (135, N'Taiwan', 35980)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (136, N'Moldova', 33843)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (137, N'Belgium', 32545)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (138, N'Lesotho', 30355)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (139, N'Armenia', 29800)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (140, N'Albania', 28748)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (141, N'Solomon Islands', 28450)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (142, N'Equatorial Guinea', 28051)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (143, N'Burundi', 27834)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (144, N'Haiti', 27750)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (145, N'Israel (Including West Bank and Gaza)', 26990)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (146, N'Rwanda', 26338)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (147, N'Macedonia', 25333)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (148, N'Belize', 22966)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (149, N'Djibouti', 22000)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (150, N'El Salvador', 21040)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (151, N'Slovenia', 20253)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (152, N'Fiji', 18270)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (153, N'Kuwait', 17820)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (154, N'Swaziland', 17363)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (155, N'East Timor', 14874)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (156, N'Bahamas', 13940)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (157, N'Montenegro', 13812)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (158, N'Vanuatu', 12200)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (159, N'Qatar', 11437)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (160, N'The Gambia', 11300)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (161, N'Jamaica', 10990)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (162, N'Kosovo', 10908)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (163, N'Lebanon', 10452)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (164, N'Cyprus', 9250)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (165, N'Brunei', 5770)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (166, N'Trinidad and Tobago', 5128)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (167, N'Cape Verde', 4033)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (168, N'Samoa', 2860)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (169, N'Luxembourg', 2586)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (170, N'Comoros', 2170)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (171, N'Mauritius', 1860)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (172, N'S?o Tom? and Pr?ncipe', 1001)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (173, N'Dominica', 754)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (174, N'Tonga', 748)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (175, N'Kiribati', 717)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (176, N'Micronesia', 702)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (177, N'Singapore', 692)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (178, N'Bahrain', 665)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (179, N'Saint Lucia', 620)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (180, N'Seychelles', 455)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (181, N'Andorra', 468)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (182, N'Palau', 458)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (183, N'Antigua and Barbuda', 442)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (184, N'Barbados', 430)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (185, N'Saint Vincent and the Grenadines', 389)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (186, N'Grenada', 340)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (187, N'Malta', 316)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (188, N'Maldives', 300)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (189, N'Saint Kitts and Nevis', 261)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (190, N'Marshall Islands', 181)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (191, N'Liechtenstein', 160)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (192, N'San Marino', 61)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (193, N'Tuvalu', 26)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (194, N'Nauru', 21)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (195, N'Monaco', 1)
+GO
+INSERT CountriesByArea (Position, Country, KmSquared) VALUES (196, N'Vatican City', 1)
+GO
+
+-- show results
+SELECT * FROM CountriesByArea
